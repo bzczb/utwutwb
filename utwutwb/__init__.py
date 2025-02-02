4a47ee3a981ea651d8d39efd02d532604cf0ab3b
vermont <uqzx@protonmail.com>
2025-02-01 22:28:18 -0500

    Update __init__.py

---
+++ utwutwb/__init__.py
@@ -0,0 +1,12 @@
+import utwutwb.condition as cond
+from utwutwb.index import HashIndex, InvertedArrayIndex, InvertedIndex, RangeIndex
+from utwutwb.wut import Wut
+
+__all__ = [
+    'cond',
+    'HashIndex',
+    'InvertedArrayIndex',
+    'InvertedIndex',
+    'RangeIndex',
+    'Wut',
+]
