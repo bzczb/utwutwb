4804e7f3279b84873b3eaf010444a69c23f06280
vermont <uqzx@protonmail.com>
2025-02-02 10:53:21 -0500

    Slightly harder benchmark

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -21,6 +21,7 @@ print('Python version:\t', sys.version)
 @dataclass(frozen=True)
 class X:
     a: int
+    b: int
 
     def __hash__(self):
         return hash(id(self))
@@ -32,38 +33,46 @@ def measure(func, n=100):
 
 
 def lookup(objs):
-    iset = Wut(objs, indexes=['a'])
+    iset = Wut(objs, indexes=['a', 'b'])
 
     db = sqlite3.connect(':memory:')
-    db.execute('CREATE TABLE objs (id INT PRIMARY KEY, a INT)')
+    db.execute('CREATE TABLE objs (id INT PRIMARY KEY, a INT, b INT)')
     db.execute('CREATE INDEX idx_a ON objs (a)')
+    db.execute('CREATE INDEX idx_b ON objs (b)')
     db.executemany(
-        'INSERT INTO objs (id, a) VALUES (?, ?)', [(i, x.a) for i, x in enumerate(objs)]
+        'INSERT INTO objs (id, a, b) VALUES (?, ?, ?)',
+        [(i, x.a, x.b) for i, x in enumerate(objs)],
     )
 
-    dex = Dex(objs, ['a'])
-    fdex = FrozenDex(objs, ['a'])
+    a_and_b = True  # true -> SQLite becomes the worst by quite a bit
+    if a_and_b:
+        q = 'a = 0 AND b = 59'
+        ducks_q = {'a': 0, 'b': 59}
+    else:
+        q = 'a = 0'
+        ducks_q = {'a': 0}
 
-    plan = iset.optimize(iset.plan('a = 0'))
+    dex = Dex(objs, ['a', 'b'])
+    fdex = FrozenDex(objs, ['a', 'b'])
+
+    plan = iset.optimize(iset.plan(q))
 
     preplanned_result = []
     preplanned_measure = measure(lambda: preplanned_result.append(iset.execute(plan)))
 
     return {
-        'utwutwb IndexedSet': measure(lambda: iset.filter('a = 0')),
+        'utwutwb IndexedSet': measure(lambda: iset.filter(q)),
         'utwutwb IndexedSet (pre-planned)': preplanned_measure,
-        'utwutwb objects out': measure(
-            lambda: list(iset.objects(preplanned_result[0]))
-        ),
-        'ducks Dex': measure(lambda: set(dex[{'a': 0}])),
-        'ducks FrozenDex': measure(lambda: set(fdex[{'a': 0}])),
+        'utwutwb objects out': measure(lambda: iset.list_objects(preplanned_result[0])),
+        'ducks Dex': measure(lambda: set(dex[ducks_q])),
+        'ducks FrozenDex': measure(lambda: set(fdex[ducks_q])),
         'sqlite': measure(
             lambda: {
                 objs[i]
-                for (i,) in db.execute('SELECT id FROM objs WHERE a = 0').fetchall()
+                for (i,) in db.execute(f'SELECT id FROM objs WHERE {q}').fetchall()
             }
         ),
-        # "set comprehension": measure(lambda: {x for x in objs if x.a == 0}),
+        # "set comprehension": measure(lambda: {x for x in objs if x.a == 0 and x.b == 59}),
     }
 
 
@@ -81,27 +90,36 @@ def graph(x, data, title, xlabel):
 
 
 n = [1, 10, 100, 1000]
-results = [lookup([X(a=j % (100_000 // i)) for j in range(100_000)]) for i in n]
+results = [lookup([X(a=j % (100_000 // i), b=59) for j in range(100_000)]) for i in n]
 graph(n, results, 'Match objects', 'Number of matching objects')
 
 
 def range_lookup(objs, start, stop):
-    iset = Wut(objs, indexes=['a'])
+    iset = Wut(objs, indexes=['a', 'b'])
 
     db = sqlite3.connect(':memory:')
-    db.execute('CREATE TABLE objs (id INT PRIMARY KEY, a INT)')
+    db.execute('CREATE TABLE objs (id INT PRIMARY KEY, a INT, b INT)')
     db.execute('CREATE INDEX idx_a ON objs (a)')
+    db.execute('CREATE INDEX idx_b ON objs (b)')
     db.executemany(
-        'INSERT INTO objs (id, a) VALUES (?, ?)', [(i, x.a) for i, x in enumerate(objs)]
+        'INSERT INTO objs (id, a, b) VALUES (?, ?, ?)',
+        [(i, x.a, x.b) for i, x in enumerate(objs)],
     )
 
-    dex = Dex(objs, ['a'])
+    dex = Dex(objs, ['a', 'b'])
 
-    fdex = FrozenDex(objs, ['a'])
+    fdex = FrozenDex(objs, ['a', 'b'])
 
-    ducks_q = {'a': {'>=': start, '<': stop}}
+    a_and_b = True
+    if a_and_b:
+        q = f'a >= {start} AND a < {stop} AND b = 59'
+        ducks_q = {'a': {'>=': start, '<': stop}, 'b': 59}
+        set_comp_q = lambda x: x.a >= start and x.a < stop and x.b == 59  # noqa
+    else:
+        q = f'a >= {start} AND a < {stop}'
+        ducks_q = {'a': {'>=': start, '<': stop}}
+        set_comp_q = lambda x: x.a >= start and x.a < stop  # noqa
 
-    q = f'a >= {start} AND a < {stop}'
     plan = iset.optimize(iset.plan(q))
 
     preplanned_result = []
@@ -110,9 +128,7 @@ def range_lookup(objs, start, stop):
     return {
         'utwutwb IndexedSet': measure(lambda: iset.filter(q)),
         'utwutwb IndexedSet (pre-planned)': preplanned_measure,
-        'utwutwb objects out': measure(
-            lambda: list(iset.objects(preplanned_result[0]))
-        ),
+        'utwutwb objects out': measure(lambda: iset.list_objects(preplanned_result[0])),
         'ducks Dex': measure(lambda: set(dex[ducks_q])),
         'ducks FrozenDex': measure(lambda: set(fdex[ducks_q])),
         'sqlite': measure(
@@ -123,14 +139,12 @@ def range_lookup(objs, start, stop):
                 ).fetchall()
             }
         ),
-        'set comprehension': measure(
-            lambda: {x for x in objs if x.a >= start and x.a < stop}
-        ),
+        'set comprehension': measure(lambda: {x for x in objs if set_comp_q(x)}),
     }
 
 
 n = [100, 1000, 10000, 50000]
-range_results = [range_lookup([X(a=j) for j in range(100_000)], 0, i) for i in n]
+range_results = [range_lookup([X(a=j, b=59) for j in range(100_000)], 0, i) for i in n]
 graph(n, range_results, 'Match range of objects', 'Number of matching objects')
 
 sys.exit(0)
